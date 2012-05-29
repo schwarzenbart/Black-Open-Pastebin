73b77fe089ade3a74bfcf904d4370f027666f762
schwarzenbart <blackbeard@tormail.org>
2012-05-29 15:37:32 +0100

    style issue

--- index.php
+++ index.php
@@ -1732,8 +1732,8 @@ if($requri != "install")
 				.alternate { background-color: #F3F3F3; }
 				.plainText { font-family: Arial, Helvetica, sans-serif; border: none; list-style-type: none; margin-bottom: 25px; }
 				.monoText { font-family:"Courier New",Courier,mono; list-style-type: decimal; }
-				.pastedImage { max-width: 100%; height : auto; }
-				.pastedImage { width: auto; max-height : 100%; }
+				.pastedImage { max-width: 95%; height : auto; }
+				.pastedImage { width: auto; max-height : 95%; }
 				.infoMessage { padding: 25px; font-size: medium; max-width: 800px; }
 				.lineHighlight { background-color: #FFFFAA; font-weight: bolder; color: #000000; }
 				.pasteEnterLabel { width: 80%; display: block; }
